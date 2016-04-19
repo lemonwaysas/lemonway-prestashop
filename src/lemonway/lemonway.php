f5db594c8c1f7ce7cfde01a28e400baca1c05288
Kassim Belghait <kassim@sirateck.com>
2016-04-19 16:18:59 +0200

    up module version to 1.1.3

--- src/lemonway/lemonway.php
+++ src/lemonway/lemonway.php
@@ -49,7 +49,7 @@ class Lemonway extends PaymentModule
     {
         $this->name = 'lemonway';
         $this->tab = 'payments_gateways';
-        $this->version = '1.1.2';
+        $this->version = '1.1.3';
         $this->author = 'SIRATECK';
         $this->need_instance = 0;
 
