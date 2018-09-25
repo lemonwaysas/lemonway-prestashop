7eba53ac0d05ffed7072f21aea482fcd967ce73c
Dat Pham <dpham@lemonway.com>
2018-09-25 17:30:59 +0200

    Fix bug sql

--- src/lemonway/upgrade/upgrade-1.4.1.php
+++ src/lemonway/upgrade/upgrade-1.4.1.php
@@ -45,11 +45,15 @@ function upgrade_module_1_4_1($module)
     $os->color = "#4169E1";
     $os->save();
 
-    $query = "ALTER TABLE `" . _DB_PREFIX_ . "lemonway_wktoken`
-        ADD `is_order_validated` tinyint(1) UNSIGNED  NOT NULL DEFAULT '0'
-        ENGINE=" . _MYSQL_ENGINE_ . " DEFAULT CHARSET=utf8;";
+    $query = "SELECT `is_order_validated` FROM `" . _DB_PREFIX_ . "lemonway_wktoken`";
+    $res = Db::getInstance()->executeS($query);
 
-    Db::getInstance()->execute($query);
+    if (!$res) {
+        $query = "ALTER TABLE `" . _DB_PREFIX_ . "lemonway_wktoken`
+            ADD `is_order_validated` tinyint(1) UNSIGNED NOT NULL DEFAULT '0'";
+
+        Db::getInstance()->execute($query);
+    }
 
     return true;
 }
